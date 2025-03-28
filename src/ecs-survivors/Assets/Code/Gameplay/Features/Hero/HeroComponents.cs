fdb3def7f15feaf36d7d972796a7b10c7ec10c55
Dmitry Marchenko <dmitriy.marchenko@headyapps.com>
2025-03-28 09:34:59 +0300

    feat: add hero movement

---
+++ src/ecs-survivors/Assets/Code/Gameplay/Features/Hero/HeroComponents.cs
@@ -0,0 +1,11 @@
+﻿using Code.Gameplay.Features.Hero.Behaviours;
+using Entitas;
+
+namespace Code.Gameplay.Features.Hero
+{
+    public class HeroComponents
+    {
+        [Game] public class Hero : IComponent {  }
+        [Game] public class HeroAnimatorComponent : IComponent {public  HeroAnimator Value;}
+    }
+}
---
+++ src/ecs-survivors/Assets/Code/Gameplay/Features/Hero/Registrars/HeroRegistrar.cs
@@ -0,0 +1,29 @@
+﻿using Code.Common.Entity;
+using Code.Common.Extensions;
+using Code.Gameplay.Features.Hero.Behaviours;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Hero.Registrars
+{
+    public class HeroRegistrar : MonoBehaviour
+    {
+        public float speed = 2f;
+        public Vector2 direction = Vector2.zero;
+        public HeroAnimator heroAnimator;
+        private GameEntity _entity;
+
+        private void Awake()
+        {
+            _entity = CreateEntity.Empty()
+                .AddTransform(transform)
+                .AddWorldPosition(transform.position)
+                .AddSpeed(speed)
+                .AddHeroAnimator(heroAnimator)
+                .AddSpriteRenderer(heroAnimator.SpriteRenderer)
+                .AddDirection(direction)
+                .With(x => x.isHero = true)
+                .With(x => x.isTurnedAlongDirection = true)
+                ;
+        }
+    }
+}
