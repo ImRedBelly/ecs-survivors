0dea9c1c85fa87ca7f32ddc0e6e293e58e4771d9
Dmitry Marchenko <dmitriy.marchenko@headyapps.com>
2025-03-28 09:34:59 +0300

    feat: add hero movement

---
+++ src/ecs-survivors/Assets/Code/Gameplay/Input/InputComponents.cs
@@ -0,0 +1,11 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace Code.Gameplay.Input
+{
+    public class InputComponents
+    {
+        [Game] public class Input : IComponent {  }
+        [Game] public class AxisInput : IComponent { public Vector2 Value; }
+    }
+}
